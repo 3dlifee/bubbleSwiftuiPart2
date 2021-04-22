e3e8798f8b080ce5be477d57279a1fa8b6414d3f
3dlifeStudio <mjfernandez3d@gmail.com>
2021-04-22 14:58:27 +0100

    Initial Commit

---
+++ bubbleSwiftuiPart2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bubbleSwiftuiPart2
+//
+//  Created by Mario Fernandes on 22/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bubbleSwiftuiPart2/bubbleSwiftuiPart2App.swift
@@ -0,0 +1,17 @@
+//
+//  bubbleSwiftuiPart2App.swift
+//  bubbleSwiftuiPart2
+//
+//  Created by Mario Fernandes on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct bubbleSwiftuiPart2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
